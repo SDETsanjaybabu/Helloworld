d0a25332b83af47fa83dae73f762088db8f95fff
sanjaybabu123 <sanjaybabu700@gmail.com>
2022-05-12 17:10:47 +0530

    hi

---
+++ verySimple/src/test/java/simple/Simple1Test.java
@@ -0,0 +1,21 @@
+package simple;
+
+import org.testng.annotations.Test;
+
+public class Simple1Test {
+	@Test
+	public void hi()
+	{
+		System.out.println("hi guys");
+		hello();
+	}
+	public void hello()
+	{
+		System.out.println("hello guys");
+	}
+	public void demo()
+	{
+		System.out.println("bye");
+		System.out.println("bye bye");
+	}
+}
